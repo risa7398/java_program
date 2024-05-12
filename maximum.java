53a34551cfac35ec179fd7abf0cfb919f557d628
risa7398 <167171159+risa7398@users.noreply.github.com>
2024-05-12 20:21:30 +0530

    Add files via upload

---
+++ maximum.java
@@ -0,0 +1,21 @@
+package basic;
+import java.util.*;
+
+public class maximum {
+
+	public static void main(String[] args) {
+		int a;
+		Scanner sc=new Scanner(System.in);
+		 a=sc.nextInt();
+		 System.out.println("enter the value of a="+a);
+		 if (a>0) {
+			 System.out.println("maximum number");
+		 }
+			 else  {
+				 System.out.println("is not a maximum number");
+			 }
+
+
+	}
+
+}
