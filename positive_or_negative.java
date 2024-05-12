4e258bbbf823025737240b6dfa2eca2da4bb802d
risa7398 <167171159+risa7398@users.noreply.github.com>
2024-05-12 20:23:22 +0530

    Add files via upload

---
+++ positive_or_negative.java
@@ -0,0 +1,21 @@
+package basic;
+import java.util.*;
+public class positive_or_negative {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int a;
+		System.out.println("enter the number=");
+		a=sc.nextInt();
+		if (a>0) {
+			System.out.println("is a positive number");
+		}
+		else {
+			System.out.println("is negative number");
+		}
+		
+
+
+
+	}
+}
